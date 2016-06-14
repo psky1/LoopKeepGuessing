4f18001deef47a3cdc2142205389784d2e60471d
oracle <oracle@GBTC441R03RH.(none)>
2016-06-14 15:48:29 -0400

    init commit

---
+++ src/KeepGuessing.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class KeepGuessing {
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		int secretNumber, guess = 0;//guess is initialized to 0
+		
+		secretNumber = 123;
+		
+		System.out.println("I'm thinking of a number between 1 and 1000");
+		System.out.print("Enter the number:");
+		guess = keyboard.nextInt();
+		
+		while ( guess != secretNumber ) 
+		{
+			if (guess == 5) break;
+			System.out.println("\nYou are wrong. Try again.");
+			System.out.println("Enter the number: ");
+			guess = keyboard.nextInt();
+		}
+		if (guess == 5)
+				System.out.println("we don't take the number 5");
+				else
+		        System.out.println("You are correct. You win a prize!");
+		keyboard.close();
+	}
+}
